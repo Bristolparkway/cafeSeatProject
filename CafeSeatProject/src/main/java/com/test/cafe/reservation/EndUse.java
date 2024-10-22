32a555fc0ecbf41733c894d8bcfae0b92daea33f
bristolparkway javaprince <sangsongsoo@gmail.com>
2024-10-22 17:41:07 +0900

    10/22/24 서블릿이름 변경

--- CafeSeatProject/src/main/java/com/test/cafe/reservation/EndUse.java
+++ CafeSeatProject/src/main/java/com/test/cafe/reservation/EndUse.java
@@ -11,15 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 
 //doGet -> 이용 종료 화면 호출
 //doPost -> 이용 종료 처리
-@WebServlet("/reservation/terminateseat.do")
-public class TerminateSeat extends HttpServlet {
+@WebServlet("/reservation/enduse.do")
+public class EndUse extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		
 
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/reservation/terminateseat.jsp");
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/reservation/enduse.jsp");
 		dispatcher.forward(req, resp);
 	}
 }
